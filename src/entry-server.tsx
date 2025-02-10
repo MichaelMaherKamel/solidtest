0393dc51f7215d747fb42ebe9a8e0a7121e78257
MichaelMaherKamel <michaelmaherkamel@gmail.com>
2025-02-10 19:21:38 +0200

    Adding Google Analytics

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -9,6 +9,16 @@ export default createHandler(() => (
           <meta charset='utf-8' />
           <meta name='viewport' content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' />
           <link rel='icon' href='/favicon.ico' />
+          {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-QGL20MWGGB"></script>
+          <script>
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-QGL20MWGGB');
+            `}
+          </script>
           {assets}
         </head>
         <body>
